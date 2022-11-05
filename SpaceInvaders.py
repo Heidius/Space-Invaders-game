5db3d4d3a3ef2526152061e4709598cb2c631a52
Ignacio BG <73847888+Heidius@users.noreply.github.com>
2022-11-05 14:39:37 +0000

    Update SpaceInvaders.py

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -37,21 +37,21 @@ class Ship:  # OOP -> This is the papa class
         self.y = y
         self.health = health
         self.ship_img = None
-        self.lasers_img = None
-        self.lasers = []
+        self.laser_img = None
+        self.laser = []
         self.cool_down_counter = 0  # Cool down for the lasers
     
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
-        for laser in self.lasers:
+        for laser in self.laser:
             laser.draw(window)
     
-    def move_lasers(self, vel, obj):
+    def move_laser(self, vel, obj):
         self.cooldown()
-        for laser in self.lasers:
+        for laser in self.laser:
             laser.move(vel)
             if laser.off_screen(HEIGHT):
-                self.lasers.remove(laser)
+                self.laser.remove(laser)
             elif laser.collision(obj):
                 obj.health -= 10
                 self.laser.remove(laser)
@@ -65,7 +65,7 @@ class Ship:  # OOP -> This is the papa class
     def shoot(self):
         if self.cool_down_counter == 0:
             laser = Laser(self.x, self.y, self.laser_img)
-            self.lasers.append(laser)
+            self.laser.append(laser)
             self.cool_down_counter = 1
 
     def get_width(self):
@@ -82,18 +82,18 @@ class Player(Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)  # Mask works with pixel collision
         self.max_health = health  
 
-    def move_lasers(self, vel, objs):
+    def move_laser(self, vel, objs):
         self.cooldown()
-        for laser in self.lasers:
+        for laser in self.laser:
             laser.move(vel)
             if laser.off_screen(HEIGHT):
-                self.lasers.remove(laser)
+                self.laser.remove(laser)
             else:
                 for obj in objs:
                     if laser.collision(obj):
                         objs.remove(obj)
-                        if laser in self.lasers:
-                            self.lasers.remove(laser) 
+                        if laser in self.laser:
+                            self.laser.remove(laser) 
     
     def draw(self, window):
         super().draw(window)
@@ -118,7 +118,7 @@ class Enemy(Ship):
     def shoot(self):
         if self.cool_down_counter == 0:
             laser = Laser(self.x-20, self.y, self.laser_img)
-            self.lasers.append(laser)
+            self.laser.append(laser)
             self.cool_down_counter = 1
 
 class Laser:
@@ -239,7 +239,7 @@ def main():
         #Once the enemies reach the bottom, it subtract a life.
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
-            enemy.move_lasers(laser_vel, player)
+            enemy.move_laser(laser_vel, player)
 
             #Enemies will randomly shoot twice every second    
             if random.randrange(0, 2*60) == 1:
@@ -252,7 +252,7 @@ def main():
                 lives -= 1
                 enemies.remove(enemy)
 
-        player.move_lasers(-laser_vel, enemies)
+        player.move_laser(-laser_vel, enemies)
 
 def main_menu():
     title_font = pygame.font.SysFont("comicsans", 30)
